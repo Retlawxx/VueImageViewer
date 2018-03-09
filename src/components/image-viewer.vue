b226939504e293f91b316d5da1498f147970851e
walter.laurito <walter.laurito@volunteerworld.com>
2018-03-09 14:57:27 +0100

    FEATURE: show chosen elements by user, FEATUER: add possibilty to classify images by user

--- src/components/image-viewer.vue
+++ src/components/image-viewer.vue
@@ -1,10 +1,29 @@
 <template>
   <div class="image-viewer">
+    <div id="classificationResults">
+      Hotdogs: <br /> <img class="classificationResultImage"
+                    :src="classification.src" 
+                    v-if="classification.hotdog == true"
+                    v-for="classification in classifications" 
+                    :key="classification.index" />
+      <br />
+      Not Hotdogs: <br /> <img class="classificationResultImage"
+                    :src="classification.src" 
+                    v-if="classification.hotdog == false"
+                    v-for="classification in classifications" 
+                    :key="classification.index" />
+    </div>
+    <div id="errorScore">
+      Dein Fehler-Score: {{userErrorScore}}
+    </div>
     <div>
       <button @click="goToPrev">Previous</button>
       <button @click="goToNext">Next</button>
+      <button @click="classifyImage(true)">This is a Hotdog!</button>
+      <button @click="classifyImage(false)">This is NOT a Hotdog!</button>
     </div>
     <div class="slides-inner">
+     <p>current image index: {{currentIndex}} </p>
       <transition-group name="imageView" tag="ImageView">
         <image-view class="imageView"
                       v-for="(slide, index) in slides"
@@ -23,16 +42,36 @@ export default {
     return {
       slides: [
         {
-          src: 'static/img/Agropoli.jpg'
+          src: 'static/img/random/Agropoli.jpg'
         },
         {
-          src: 'static/img/Cicerale.jpg'
+          src: 'static/img/random/Cicerale.jpg'
         },
         {
-          src: 'static/img/Cicerale2.jpg'
+          src: 'static/img/random/Cicerale2.jpg'
+        },
+        {
+          src: 'static/img/random/nothotdog1.jpg'
+        },
+        {
+          src: 'static/img/random/nothotdog2.jpg'
+        },
+        {
+          src: 'static/img/hotdogs/hotdog.jpg',
+          hotdog: true
+        },
+        {
+          src: 'static/img/hotdogs/hotdog1.jpg',
+          hotdog: true
+        },
+        {
+          src: 'static/img/hotdogs/hotdog2.jpg',
+          hotdog: true
         }
       ],
-      currentIndex: 0
+      classifications: [],
+      currentIndex: 0,
+      userErrorScore: 0
     }
   },
   components: {
@@ -40,13 +79,47 @@ export default {
   },
   methods: {
     isImageViewVisible: function (index) {
-      return Math.abs(this.currentIndex) % this.slides.length === index
+      // return Math.abs(this.currentIndex) % this.slides.length === index
+      return this.currentIndex === index;
     },
+
     goToPrev () {
       this.currentIndex--
+      if(this.currentIndex < 0 ) {
+        this.currentIndex = 0;
+      }
     },
     goToNext () {
       this.currentIndex++
+      if(this.currentIndex >= this.slides.length) {
+          this.currentIndex = this.slides.length - 1;
+      }
+    },
+    classifyImage(hotdog = false) {
+      // deep copy to prevent 
+      const currentSlideSelected = JSON.parse(JSON.stringify(this.slides[this.currentIndex]));
+
+      if(hotdog && !currentSlideSelected.hotdog
+        || !hotdog && currentSlideSelected.hotdog) {
+        this.userErrorScore++;
+      }
+
+      // check if hotdog already inside 
+      let existingClassification = null;
+      this.classifications.forEach(classification => {
+          if(classification.src == currentSlideSelected.src) {
+              existingClassification = classification
+          }
+      });
+
+      if(!existingClassification) {
+        currentSlideSelected.hotdog = hotdog;
+        this.classifications.push(currentSlideSelected);
+
+      } else {
+        existingClassification.hotdog = hotdog;
+      }
+
     }
   }
 }
@@ -77,5 +150,13 @@ export default {
   .imageView-move {
     opacity: 1;
     transition: all 0.5s;
+  }
+  #classificationResults {
+    margin-top: 1em;
+    margin-bottom: 1em;
+  }
+  .classificationResultImage {
+    width: 5em;
+    padding: 1em;
   }
 </style>
